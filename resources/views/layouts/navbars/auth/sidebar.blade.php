8eb7f887b184bc487f1b1f348ef30d6482f1d2cb
Fabian Suarez <inge.fabiansuarez@gmail.com>
2023-12-05 16:54:03 -0500

    realizaod la integracion

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -1,5 +1,5 @@
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 "
-    id="sidenav-main">
+    style="height: calc(100vh - 0px) !important; "  id="sidenav-main">
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
@@ -300,7 +300,7 @@
             </li> --}}
         </ul>
     </div>
-    <div class="sidenav-footer mx-3 ">
+   {{--  <div class="sidenav-footer mx-3 ">
         <div class="card card-background shadow-none card-background-mask-secondary" id="sidenavCard">
             <div class="full-background"
                 style="background-image: url('../assets/img/curved-images/white-curved.jpeg')"></div>
@@ -318,5 +318,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 </aside>
